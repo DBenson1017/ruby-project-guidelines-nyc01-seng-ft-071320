e3732c32b8d74b4df6ced578622e8d2c59301be5
brianwlego <legobw@gmail.com>
2020-07-27 17:54:12 -0400

    updated airbnb key

---
+++ app/model/hotel.rb
@@ -0,0 +1,9 @@
+class Hotel 
+
+
+
+
+
+
+  
+end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 require 'bundler'
 Bundler.require
 
+require 'uri'
+require 'net/http'
+require 'openssl'
+
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
