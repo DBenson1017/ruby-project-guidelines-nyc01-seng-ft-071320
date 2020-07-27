0b1d8f329d44e4a9f66a5ff22e802576fe2387cb
brianwlego <legobw@gmail.com>
2020-07-27 18:18:01 -0400

    added functionality of API to hotel class

--- app/model/hotel.rb
+++ app/model/hotel.rb
@@ -1,9 +1,21 @@
+
 class Hotel 
 
 
 
+    url = URI("https://airbnb-com.p.rapidapi.com/listings/nearby/40.750134/-73.997009")
 
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
+  request = Net::HTTP::Get.new(url)
+  request["x-rapidapi-host"] = 'airbnb-com.p.rapidapi.com'
+  request["x-rapidapi-key"] = ENV['airbnb_key']
 
-  
+  #response = http.request(request)
+  data = JSON.parse(http.request(request).body)
+
+  binding.pry
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,17 @@
 require 'bundler'
-Bundler.require
-
 require 'uri'
 require 'net/http'
 require 'openssl'
+require 'json'
+require 'pry'
+require 'dotenv/load'
+Bundler.require
+
+require_all 'app/model'
+
+
+
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
+
