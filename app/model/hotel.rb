80966861edc86df44e32cc5b5f5b3bdc451beeb9
brianwlego <legobw@gmail.com>
2020-07-30 08:53:04 -0400

    Took off Hotel.accoms

--- app/model/hotel.rb
+++ app/model/hotel.rb
@@ -5,11 +5,7 @@ class Hotel < ActiveRecord::Base
     has_many :reservations 
     has_many :users, through: :reservations 
 
-    def self.all_accomidations
-        Hotel.all.each do |r|
-        puts "Id: #{r.id}\nName: #{r.name}\nPrice: #{r.price}\nBeds: #{r.beds}\nGuest Count: #{r.guest_amount}\nNeighborhood: #{r.neighborhood}\n\n"
-        end
-      end
+
 
 
 end
